9b2f6a7a65255148921f24ede171184faf403324
Brux-Stream <brux.stream@gmail.com>
2025-04-29 02:18:13 +0200

    “NoBox”

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 /* Animacije deley */
 
 document.addEventListener("DOMContentLoaded", function() {
-    let observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('animiraj-start');
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const targetAnimacije = entry.target.querySelector('.animacije');
+        if (targetAnimacije) {
+          targetAnimacije.classList.add('animiraj-start');
         }
-      });
-    }, { threshold: 0.5 });
-
-    document.querySelectorAll('.animiraj-me').forEach(el => {
-      observer.observe(el);
+      }
     });
+  }, { threshold: 0.5 });
+
+  document.querySelectorAll('.sekcija').forEach(section => {
+    observer.observe(section);
   });
+});
